7bdb936bc3ae69117e80e46ff88013660ec427ca
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-03-21 13:18:58 -0300

    remodelando

---
+++ AutiCare/Modelos/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AutiCare
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public short sexo { get; set; }
+
+    }
+}
