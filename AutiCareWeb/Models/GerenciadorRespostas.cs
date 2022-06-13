3654956a3153d0f28b7886f0713f33509076af22
Ana Flávia Rodrigues <78914063+Ana-F-Rodrigues@users.noreply.github.com>
2022-06-13 10:43:54 -0300

    melhorias

--- AutiCareWeb/Models/GerenciadorRespostas.cs
+++ AutiCareWeb/Models/GerenciadorRespostas.cs
@@ -17,21 +17,21 @@ namespace AutiCareWeb.Models
         public static void AdicionarRespostasDiagnostico(Diagnostico diagnostico)
         {
             TableDiagnostico.Etnia = (short)diagnostico.Etnia;
-            TableDiagnostico.Idade = (short)diagnostico.Idade;
+            TableDiagnostico.Idade = diagnostico.Idade;
             TableDiagnostico.Sexo = (short)diagnostico.Sexo;
-            TableDiagnostico.Ictericia = (bool)diagnostico.Ictericia;
-            TableDiagnostico.FamiliarPDD = (bool)diagnostico.FamiliarPDD;
+            TableDiagnostico.Ictericia = diagnostico.Ictericia;
+            TableDiagnostico.FamiliarPDD = diagnostico.FamiliarPDD;
             TableDiagnostico.TipoUser = (short)diagnostico.TipoUser;
-            TableDiagnostico.Pergunta_1 = (bool)diagnostico.Pergunta_1;
-            TableDiagnostico.Pergunta_2 = (bool)diagnostico.Pergunta_2;
-            TableDiagnostico.Pergunta_3 = (bool)diagnostico.Pergunta_3;
-            TableDiagnostico.Pergunta_4 = (bool)diagnostico.Pergunta_4;
-            TableDiagnostico.Pergunta_5 = (bool)diagnostico.Pergunta_5;
-            TableDiagnostico.Pergunta_6 = (bool)diagnostico.Pergunta_6;
-            TableDiagnostico.Pergunta_7 = (bool)diagnostico.Pergunta_7;
-            TableDiagnostico.Pergunta_8 = (bool)diagnostico.Pergunta_8;
-            TableDiagnostico.Pergunta_9 = (bool)diagnostico.Pergunta_9;
-            TableDiagnostico.Pergunta_10 = (bool)diagnostico.Pergunta_10;
+            TableDiagnostico.Pergunta_1 = diagnostico.Pergunta_1;
+            TableDiagnostico.Pergunta_2 = diagnostico.Pergunta_2;
+            TableDiagnostico.Pergunta_3 =diagnostico.Pergunta_3;
+            TableDiagnostico.Pergunta_4 = diagnostico.Pergunta_4;
+            TableDiagnostico.Pergunta_5 = diagnostico.Pergunta_5;
+            TableDiagnostico.Pergunta_6 = diagnostico.Pergunta_6;
+            TableDiagnostico.Pergunta_7 = diagnostico.Pergunta_7;
+            TableDiagnostico.Pergunta_8 = diagnostico.Pergunta_8;
+            TableDiagnostico.Pergunta_9 = diagnostico.Pergunta_9;
+            TableDiagnostico.Pergunta_10 = diagnostico.Pergunta_10;
 
         }
 
